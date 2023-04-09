cee702c55029521f1df8dc2c5e030feb5082d0c3
Lare Claudiu-Darius <123365981+Darius-Lr@users.noreply.github.com>
2023-04-09 19:48:37 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function myFunction()
+{
+  var element = document.getElementById("bot");
+  element.classList.remove("invisible");
+  var element2 = document.getElementById("bot2");
+  element2.classList.remove("invisible");
+}
